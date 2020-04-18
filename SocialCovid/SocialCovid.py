b4b00e6b6c6100839737b9028835e33e1b7f1331
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2020-04-18 17:21:02 +0100

    format files

--- SocialCovid/SocialCovid.py
+++ SocialCovid/SocialCovid.py
@@ -5,6 +5,25 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 import os
 
+def plot(node, subg):
+	color_map = []
+	for subnode in subg:
+	    if subnode == node:
+	        color_map.append('#ffaaaa')
+	    else: 
+	        color_map.append('#aaaaff')
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	rootNode = str(node)
+	fig.suptitle('Infection source: ' + rootNode)
+	infected = str(subg.number_of_nodes() - 1)
+	ax.set_title('Total infected: ' + infected)
+	ax.axis('off')
+	nx.draw_networkx(subg, node_size = 100, node_color = color_map, edge_color = '#aaaaaa', font_size = 6)
+	#plt.show()
+	plt.savefig('subgraphs/' + infected + "_" + rootNode + '.png', dpi = 300)
+	plt.close()
+
 def main():
     # load patient csv data
     patientInfo = pd.read_csv('data/PatientInfo.csv')
@@ -27,24 +46,8 @@ def main():
     for node in list(g):
         if (g.in_degree(node) == 0):
             print(node)
-            subg = g.subgraph(nx.descendants(g, node)|{node})
-            color_map = []
-            for subnode in subg:
-                if subnode == node:
-                    color_map.append('#ffaaaa')
-                else: 
-                    color_map.append('#aaaaff')
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-            rootNode = str(node)
-            fig.suptitle('Infection source: ' + rootNode)
-            infected = str(subg.number_of_nodes() - 1)
-            ax.set_title('Total infected: ' + infected)
-            ax.axis('off')
-            nx.draw_networkx(subg, node_size = 100, node_color = color_map, edge_color = '#aaaaaa', font_size = 6)
-            #plt.show()
-            plt.savefig('subgraphs/'+ infected + "_" + rootNode + '.png', dpi = 300)
-            plt.close()
+            subg = g.subgraph(nx.descendants(g, node) | {node})
+            plot(node, subg)
 
 
 if __name__ == "__main__":
