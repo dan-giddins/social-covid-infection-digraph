d7893bd5e1bc04d3e6eec10fd221cebce20d8d06
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2020-04-18 18:33:40 +0100

    start work on proximity

--- SocialCovid/SocialCovid.py
+++ SocialCovid/SocialCovid.py
@@ -4,51 +4,100 @@ import math
 import matplotlib.pyplot as plt
 from matplotlib import pylab
 import os
+import geopy.distance
+import pickle
 
-def plot(node, subg):
-	color_map = []
-	for subnode in subg:
-	    if subnode == node:
-	        color_map.append('#ffaaaa')
-	    else: 
-	        color_map.append('#aaaaff')
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	rootNode = str(node)
-	fig.suptitle('Infection source: ' + rootNode)
-	infected = str(subg.number_of_nodes() - 1)
-	ax.set_title('Total infected: ' + infected)
-	ax.axis('off')
-	nx.draw_networkx(subg, node_size = 100, node_color = color_map, edge_color = '#aaaaaa', font_size = 6)
-	#plt.show()
-	plt.savefig('subgraphs/' + infected + "_" + rootNode + '.png', dpi = 300)
-	plt.close()
+
+INFECTED_ENABLE = False
+INFECTED_FOLDER = 'subgraphs_infected'
+INFECTED_PLOT = False
+PROXIMITY_ENABLE = True
+PROXIMITY_VALUE = 0.01
+PROXIMITY_FOLDER = 'subgraphs_proximity_' + str(PROXIMITY_VALUE)
+PROXIMITY_PLOT = True
+
+
+def plot_infected(node, subg):
+    color_map = []
+    for subnode in subg:
+        if subnode == node:
+            color_map.append('#ffaaaa')
+        else: 
+            color_map.append('#aaaaff')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    rootNode = str(node)
+    fig.suptitle('Infection source: ' + rootNode)
+    infected = str(subg.number_of_nodes() - 1)
+    ax.set_title('Total infected: ' + infected)
+    ax.axis('off')
+    nx.draw_networkx(subg, node_size = 100, node_color = color_map, edge_color = '#aaaaaa', font_size = 6)
+    plt.savefig(INFECTED_FOLDER + '/' + infected + "_" + rootNode + '.png', dpi = 300)
+    plt.close()
+
+def proximity(node1Locations, node2Locations):
+    return geopy.distance.distance(node1Locations, node2Locations).km
+
+def check_locations(locations, node1, node2, proximityGraph):
+    for node1Locations in locations[node1]:
+        for node2Locations in locations[node2]:
+            if proximity(node1Locations, node2Locations) <= PROXIMITY_VALUE:
+                proximityGraph.add_edge(node1, node2)
+                print((node1, node2))
+                return
 
 def main():
+    # create subgraph dirs
+    if not os.path.exists(INFECTED_FOLDER):
+        os.makedirs(INFECTED_FOLDER)
+    if not os.path.exists(PROXIMITY_FOLDER):
+        os.makedirs(PROXIMITY_FOLDER)
+
     # load patient csv data
     patientInfo = pd.read_csv('data/PatientInfo.csv')
     patientRoute = pd.read_csv('data/PatientRoute.csv')
 
-    # create directed infection graph
-    g = nx.DiGraph()
-    for index, row in patientInfo.iterrows():
-        if not math.isnan(row['infected_by']):
-            g.add_edge(int(row['infected_by']), int(row['patient_id']))
+    if (INFECTED_ENABLE):
+        # create directed infection graph
+        infectionGraph = nx.DiGraph()
+        for index, row in patientInfo.iterrows():
+            if not math.isnan(row['infected_by']):
+                infectionGraph.add_edge(int(row['infected_by']), int(row['patient_id']))
         
-    # count nodes in graph
-    print(g.number_of_nodes())
+        # count nodes in graph
+        print(infectionGraph.number_of_nodes())
 
-    # create subgraph dir
-    if not os.path.exists('subgraphs'):
-        os.makedirs('subgraphs')
+        # find root infectors and their subgraphs
+        for node in list(infectionGraph):
+            if (infectionGraph.in_degree(node) == 0):
+                print(node)
+                subg = infectionGraph.subgraph(nx.descendants(infectionGraph, node) | {node})
+                if (INFECTED_PLOT):
+                    plot_infected(node, subg)
 
-    # find root nodes and their subgraphs
-    for node in list(g):
-        if (g.in_degree(node) == 0):
-            print(node)
-            subg = g.subgraph(nx.descendants(g, node) | {node})
-            plot(node, subg)
+    if (PROXIMITY_ENABLE):
+        # create proximity graph
+        proximityGraph = nx.Graph()
+        locations = {}
+        for index, row in patientRoute.iterrows():
+            patient_id = int(row['patient_id'])
+            if patient_id not in locations:
+                proximityGraph.add_node(patient_id)
+                locations[patient_id] = [(row['latitude'], row['longitude'])]
+            else:
+                locations[patient_id].append((row['latitude'], row['longitude']))
 
+        # add edges
+        for node1 in list(proximityGraph):
+            for node2 in list(proximityGraph):
+                if not node1 == node2:
+                    check_locations(locations, node1, node2, proximityGraph)
 
+        with open('proximityGraph', 'wb') as file:
+            pickle.dump(proximityGraph, file)
+
+        print(proximityGraph.number_of_edges)
+
+        # plot
 if __name__ == "__main__":
     main()
